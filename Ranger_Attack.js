e9395a1a846db7358c20346bce610afcdc869edc
Crowns3bc <44653214+Crowns3bc@users.noreply.github.com>
2022-08-30 12:29:57 -0600

    Update Ranger_Attack.js

--- Ranger_Attack.js
+++ Ranger_Attack.js
@@ -9,7 +9,7 @@ function farm() {
                 .filter(e => e.type == "monster")
                 .filter(e => e.target)
                 .filter((a)=>a.target=== "CrownPriest")
- 							.map(e => e.id);
+ 		.map(e => e.id);
             if( arr.length > 1 && arr.length < 4 ) use("3shot",arr);
             if( arr.length > 4 ) use("5shot",arr);
             break;

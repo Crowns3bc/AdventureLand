4a45151c8f053dfd5493cb06e3556443b76a52d6
Crowns3bc <44653214+Crowns3bc@users.noreply.github.com>
2022-08-30 12:28:56 -0600

    Create Ranger_Attack.js

---
+++ Ranger_Attack.js
@@ -0,0 +1,19 @@
+function farm() {
+	 let target = get_nearest_monster({target:"CrownPriest"});
+}
+        if(target){
+    if(can_attack(target)){
+    switch(parent.character.ctype ) {
+        case "ranger":
+             var arr = Object.values(parent.entities)
+                .filter(e => e.type == "monster")
+                .filter(e => e.target)
+                .filter((a)=>a.target=== "CrownPriest")
+ 							.map(e => e.id);
+            if( arr.length > 1 && arr.length < 4 ) use("3shot",arr);
+            if( arr.length > 4 ) use("5shot",arr);
+            break;
+        }
+      attack(target);
+  }
+}

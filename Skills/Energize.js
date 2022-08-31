6ff0f24315439db612e69f32b73089ef7b85e2c0
Crowns3bc <44653214+Crowns3bc@users.noreply.github.com>
2022-08-31 03:27:25 -0600

    Update Energize.js

--- Skills/Energize.js
+++ Skills/Energize.js
@@ -1,6 +1,6 @@
 setInterval(function(){
     var c1 = get_player("CrownsAnal");
-    if(c1.mp < 1600 && character.mp > (character.max_mp * 0.7)){
+    if(c1.mp < 450 && character.mp > (character.max_mp * 0.7)){
         use_skill("energize", 'CrownsAnal');
     }
 }, 300);

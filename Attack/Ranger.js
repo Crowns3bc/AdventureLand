bc34734212d1a1556cb118fdd3ebbb2f045b9139
Crowns3bc <44653214+Crowns3bc@users.noreply.github.com>
2022-11-10 17:33:17 -0700

    Update Ranger.js

--- Attack/Ranger.js
+++ Attack/Ranger.js
@@ -1,19 +1,51 @@
-function farm() {
-	 let target = get_nearest_monster({target:"CrownPriest"});
+function ms_to_next_skill(skill) {
+    const next_skill = parent.next_skill[skill]
+    if (next_skill == undefined) return 0
+    const ms = parent.next_skill[skill].getTime() - Date.now() - Math.min(...parent.pings)
+    return ms < 0 ? 0 : ms
 }
-        if(target){
-    if(can_attack(target)){
-    switch(parent.character.ctype ) {
-        case "ranger":
-             var arr = Object.values(parent.entities)
+
+async function attackLoop() {
+    let delay = 5;
+    try {
+        let nearest = getNearestMonster({target: ["CrownPriest", "CrownTown"]});
+        if (can_attack(nearest)) {
+			var arr = Object.values(parent.entities)
                 .filter(e => e.type == "monster")
                 .filter(e => e.target)
-                .filter((a)=>a.target=== "CrownPriest")
- 		.map(e => e.id);
+                .filter((a)=> a.target === "CrownTown" || 
+						      a.target === "CrownPriest")
+ 				  .map(e => e.id);
             if( arr.length > 1 && arr.length < 4 ) use("3shot",arr);
             if( arr.length > 4 ) use("5shot",arr);
-            break;
+            await attack(nearest)
+
+            delay = ms_to_next_skill('attack');
         }
-      attack(target);
-  }
+    } catch (e) {
+        console.error(e)
+    }
+    setTimeout(attackLoop, delay)
+}
+attackLoop()
+
+function getNearestMonster(args) {
+    for (const id in parent.entities) {
+        const entity = parent.entities[id];
+        if (entity.type !== 'monster' || entity.dead) {
+            continue;
+        }
+        if (args.target) {
+            if (Array.isArray(args.target)) {
+                if (!args.target.includes(entity.target)) {
+                    continue;
+                }
+            } else {
+                if (entity.target !== args.target) {
+                    continue;
+                }
+            }
+        }
+        return entity;
+    }
 }

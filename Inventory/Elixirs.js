0e548283c47d7ca6472ee5bd7c40cc9a90c146ad
Crowns3bc <44653214+Crowns3bc@users.noreply.github.com>
2022-11-10 17:45:38 -0700

    Update Elixirs.js

--- Inventory/Elixirs.js
+++ Inventory/Elixirs.js
@@ -1,8 +1,6 @@
-setInterval(function()
-{
-    if(character.slots.elixir == null)
-    {
+setInterval(function(){
+    if(character.slots.elixir == null){
         game_log("Drinking Elixir");
-        use(5);
+        use(5); //location of elixir in your inventory
     }
 },20*1000);

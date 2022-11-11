38a5f4560a136420f3f4149fa1235c070385d8b1
Crowns3bc <44653214+Crowns3bc@users.noreply.github.com>
2022-11-10 17:42:08 -0700

    Update Priest.js

--- Attack/Priest.js
+++ Attack/Priest.js
@@ -9,24 +9,24 @@ var monster_targets =  ["plantoid", "bgoo", "rgoo", "crabxx", "franky", "icegole
 async function attackLoop() {
     let delay = 15;
     try { 
-		if(parent.is_disabled(character) == undefined){
+	if(parent.is_disabled(character) == undefined){
 	if(!character.rip){
-			if(character.hp < character.max_hp - character.heal){
-				await heal(character);
-			}
+	if(character.hp < character.max_hp - character.heal){
+		await heal(character);
+	}
             var target = find_viable_targets()[0];
 	    if(is_in_range(target)){
             await attack(target);
                     delay = ms_to_next_skill('attack');
-				}
 			}
+		}
         }
     } catch (e) {
         console.error(e)
     }
     setTimeout(attackLoop, delay)
 }
-attackLoop()
+attackLoop())
 
 function find_viable_targets() {
     var monsters = Object.values(parent.entities).filter(

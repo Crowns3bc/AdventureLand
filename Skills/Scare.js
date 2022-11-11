d98a24ebd0e78f456fe28267dc653f02855f9bc6
Crowns3bc <44653214+Crowns3bc@users.noreply.github.com>
2022-11-10 17:56:32 -0700

    Add files via upload

---
+++ Skills/Scare.js
@@ -0,0 +1,17 @@
+function scare() {
+    const slot = character.items.findIndex(i => i && i.name === "jacko")
+    const orb = character.items.findIndex(i => !i)
+     let mobnum = 0
+    for( id in parent.entities) {
+        var current = parent.entities[id];
+        if(current.type == "monster"  && current.target == 'CrownsAnal') mobnum ++;
+    }
+      if( mobnum > 0) {
+      
+          if(!is_on_cooldown("scare")) {
+              equip(slot)
+              use("scare")
+              equip(slot)
+        }
+    }
+}

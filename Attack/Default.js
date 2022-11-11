df6b301cf36f2c778b2e40c9517e20617d1b81e7
Crowns3bc <44653214+Crowns3bc@users.noreply.github.com>
2022-11-10 17:37:21 -0700

    Added custom function

--- Attack/Default.js
+++
@@ -1,9 +0,0 @@
-function kill_mobs() {
-	let target; 
-	target = get_nearest_monster();
-	 if(!parent.is_disabled(character) &&
-		is_in_range(target) &&
-		!is_on_cooldown("attack")){
-			 attack(target);
-	}
-}
---
+++ Attack/Warrior.js
@@ -0,0 +1,42 @@
+function ms_to_next_skill(skill) {
+    const next_skill = parent.next_skill[skill]
+    if (next_skill == undefined) return 0
+    const ms = parent.next_skill[skill].getTime() - Date.now() - Math.min(...parent.pings)
+    return ms < 0 ? 0 : ms
+}
+
+async function attackLoop() {
+    let delay = 10;
+    try {
+      let nearest = getNearestMonster({target: ["CrownPriest"]});
+if (is_in_range(nearest)){
+  await attack(nearest);
+          delay = ms_to_next_skill('attack');
+      }
+    } catch (e) {
+      console.error(e)
+  }
+setTimeout(attackLoop, delay)
+}
+attackLoop()
+
+function getNearestMonster(args) {
+    for (const id in parent.entities) {
+        const entity = parent.entities[id];
+        if (entity.type !== 'monster' || entity.dead) {
+            continue;
+        }
+        if (args.target) {
+            if (Array.isArray(args.target)) {
+                if (!args.target.includes(entity.target)) {
+                    continue;
+                }
+            } else {
+                if (entity.target !== args.target) {
+                    continue;
+                }
+            }
+        }
+        return entity;
+    }
+}

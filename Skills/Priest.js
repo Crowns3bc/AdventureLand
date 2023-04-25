bcf8d2d6317dc9427d6f83cfb4856ffa8d5b55d4
Crowns3bc <44653214+Crowns3bc@users.noreply.github.com>
2023-04-25 12:16:34 -0600

    changed party heal

--- Skills/Priest.js
+++ Skills/Priest.js
@@ -1,59 +1,65 @@
 async function SkillLoop() {
-    let delay = 100;
-    try { 
-	if(parent.is_disabled(character) == undefined){
-	if(!character.rip){
-	var home = get_nearest_monster({type: 'plantoid'});
-		
-  for(let x in parent.entities) {
-    let entity = parent.entities[x];
-    if(entity.mtype == "plantoid" && entity.target == undefined){
-	if(!is_on_cooldown("zapperzap")) {
-		if (character.cc <= 65){
-		if(character.mp > character.max_mp * 0.4){
-		if(character.fear < 5){
-		equip(7); //location in inventory of zapper
-      		use_skill("zapperzap", entity);
-		equip(7);
-             	        }
-    		    }
-             	}
-            }
-        }
-    }
-	    if  (character.party){
-        for (let char_name in get_party()) {
-	if (character.name == char_name) continue;
-            let monster = get_nearest_monster({target: char_name});
-            if (monster) {
-                if (!is_on_cooldown("absorb")) {
-                    use_skill("absorb", char_name);
-                }
-            }
-        }
-    }
-	var c1 = get_player("CrownPriest");
-	var c2 = get_player("CrownsAnal");
-	var c3 = get_player("CrownTown");
-	 if(c1 && c1.hp < c1.max_hp * 0.45 || 
-	   (c2 && c2.hp < c2.max_hp * 0.45) || 
-	   (c3 && c3.hp < c3.max_hp * 0.45) || 
-	   (c4 && c4.hp < c4.max_hp * 0.45)){
-		if (!is_on_cooldown("partyheal")) {
-			use_skill("partyheal");
+	let delay = 25;
+	let dead = character.rip;
+	let disabled = (parent.is_disabled(character) == undefined);
+	try {
+		for (let x in parent.entities) {
+			let entity = parent.entities[x];
+			let ready = !is_on_cooldown("zapperzap") && character.mp > G?.skills?.zapperzap?.mp + 3000 && is_in_range(entity, "zapperzap") && character.cc < 125 && character.targets < 100;
+			if (entity.mtype === "fireroamer" && entity.target == undefined ||
+				(entity.mtype === "wabbit" && entity.target == undefined)) {
+				if (!smart.moving) {
+					if (ready) {
+						if (character.slots.ring2?.name !== 'zapper') {
+							await fixPromise(equip(locate_item("zapper")))
+						}
+						await use_skill("zapperzap", entity);
+						if (character.slots.ring2?.name !== 'resistancering') {
+							await fixPromise(equip(locate_item("resistancering")))
+						}
+					}
+				}
+			}
 		}
+		if (character.party) {
+			for (let char_name in get_party()) {
+				if (character.name == char_name) continue;
+				let monster = get_nearest_monster({ target: char_name });
+				if (monster) {
+					if (!is_on_cooldown("absorb")) {
+						use_skill("absorb", char_name);
+					}
+				}
+			}
+		}
+		if (character.hp < character.max_hp * .55) {
+			await use_skill("partyheal");
+		}
+		if (character.party) {
+			for (let char_name in get_party()) {
+				if (char_name === character.name) continue; // skip self
+				if (char_name.hp < char_name.max_hp * 0.55) {
+					if (!is_on_cooldown("partyheal")) {
+						await use_skill("partyheal");
+					}
+				}
+			}
+		}
+		if (!dead || disabled) {
+			if (!is_on_cooldown("darkblessing")) {
+				use_skill("darkblessing");
+			}
+		}
+		if (!dead || disabled) {
+			if (!home.hasOwnProperty("immune") && home) {
+				if (!is_on_cooldown("curse")) {
+					use_skill("curse", home);
+				}
+			}
+		}
+	} catch (e) {
+		console.error(e)
 	}
-	if (!is_on_cooldown("darkblessing")) {
-	    use_skill("darkblessing");
-	        }
-	if (!is_on_cooldown("curse")) {
-	    use_skill("curse", home);
-                }
-            }
-	}
-    } catch (e) {
-        console.error(e)
-    }
-    setTimeout(SkillLoop, delay)
+	setTimeout(SkillLoop, delay)
 }
-SkillLoop()
+SkillLoop();

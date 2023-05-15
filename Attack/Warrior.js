a3b132bddcff4ac3583dec2527ecec69d1b58463
Crowns3bc <44653214+Crowns3bc@users.noreply.github.com>
2023-05-14 22:52:38 -0600

    Vett's Optimizations added

--- Attack/Warrior.js
+++ Attack/Warrior.js
@@ -6,24 +6,28 @@ function ms_to_next_skill(skill) {
 }
 
 async function attackLoop() {
-    let delay = 10;
+    let delay = 1;
     try {
-      let nearest = getNearestMonster({target: ["CrownPriest"]});
-if (is_in_range(nearest)){
-  await attack(nearest);
-          delay = ms_to_next_skill('attack');
-      }
+        let nearest = getNearestMonster({target: ["CrownPriest"], cursed: true});
+		if (!nearest) nearest = getNearestMonster({target: ["CrownPriest"]});
+        if (is_in_range(nearest)) {
+            await attack(nearest);
+            delay = ms_to_next_skill('attack');
+        }
     } catch (e) {
-      console.error(e)
-  }
-setTimeout(attackLoop, delay)
+        console.error(e)
+    }
+    setTimeout(attackLoop, delay)
 }
-attackLoop()
+attackLoop();
 
 function getNearestMonster(args) {
     for (const id in parent.entities) {
         const entity = parent.entities[id];
         if (entity.type !== 'monster' || entity.dead) {
+            continue;
+        }
+        if (args.cursed && !entity.s.cursed) {
             continue;
         }
         if (args.target) {

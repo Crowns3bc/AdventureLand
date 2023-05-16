cf03c8b47404551ce97fd906fa02e9c5790b6abd
Crowns3bc <44653214+Crowns3bc@users.noreply.github.com>
2023-05-16 11:15:26 -0600

    Colors and Sorter

--- Gui/DPS.js
+++ Gui/DPS.js
@@ -1,123 +1,154 @@
 var dpsInterval = 10000;
 var damageLog = [];
-setInterval(function() {
-  updateMeter();
+setInterval(function () {
+	updateMeter();
 }, 100);
+
+// Define the classColors object outside of the getFontColorByClass function
+const classColors = {
+	ranger: '#AAD372',
+	warrior: '#C69B6D',
+	priest: '#D5D5D5',
+	mage: '#3FC7EB',
+	paladin: '#F48CBA',
+	rogue: '#FFF468',
+	merchant: '#827A63',
+	// Add more class-color mappings as needed
+};
+
 function init_dpsmeter(minref) {
-  let $ = parent.$;
-  let brc = $('#bottomrightcorner');
-  brc.find('#dpsmeter').remove();
-  let dpsmeter_container = $('<div id="dpsmeter"></div>').css({
-    fontSize: '28px',
-    color: 'white',
-    textAlign: 'center',
-    display: 'table',
-    overflow: 'hidden',
-    marginBottom: '-5px',
-	width: "100%"
-  });
-  //vertical centering in css is fun
-  let xptimer = $('<div id="dpsmetercontent"></div>')
-    .css({
-      display: 'table-cell',
-      verticalAlign: 'middle'
-    })
-    .html("")
-    .appendTo(dpsmeter_container);
-  brc.children().first().after(dpsmeter_container);
-}
-function updateMeter()
-{
 	let $ = parent.$;
-	var listString = '<table border=5 bgcolor="black" align="right" cellpadding="5"><tr align="center"><td colspan="2">Damage Meter</td></tr><tr align="center"><td>Name</td><td>DPS</td></tr>';
-	if(parent.party_list != null && character.party != null)
-	{
-		for(id in parent.party_list)
-		{
-			var partyMember = parent.party_list[id];
-			var dps = getDPS(partyMember);
-			listString = listString + '<tr align="left"><td align="center">' + partyMember + '</td><td>' + dps + '</td></tr>';
+	let brc = $('#bottomrightcorner');
+	brc.find('#dpsmeter').remove();
+	let dpsmeter_container = $('<div id="dpsmeter"></div>').css({
+		fontSize: '28px',
+		color: 'white',
+		textAlign: 'center',
+		display: 'table',
+		overflow: 'hidden',
+		marginBottom: '-5px',
+		width: "100%"
+	});
+	//vertical centering in css is fun
+	let xptimer = $('<div id="dpsmetercontent"></div>')
+		.css({
+			display: 'table-cell',
+			verticalAlign: 'middle'
+		})
+		.html("")
+		.appendTo(dpsmeter_container);
+	brc.children().first().after(dpsmeter_container);
+}
+
+function updateMeter() {
+	let $ = parent.$;
+	let listString = '<table border="5" bgcolor="black" align="right" cellpadding="5"><tr align="center"><td colspan="2">Damage Meter</td></tr><tr align="center"><td>Name</td><td>DPS</td></tr>';
+	let partyMembers = [];
+
+	// Add current character to the party members array
+	let dps = getDPS(character.name);
+	let color = getFontColorByClass(character.name);
+	partyMembers.push({ name: character.name, dps, color });
+
+	if (parent.party_list != null && character.party != null) {
+		for (let className of ['ranger', 'warrior', 'priest', 'mage', 'paladin', 'rogue', 'merchant']) {
+			for (id in parent.party_list) {
+				let partyMember = parent.party_list[id];
+				if (partyMember !== character.name) {
+					let memberColor = getFontColorByClass(partyMember);
+					if (memberColor === classColors[className]) {
+						let memberDPS = getDPS(partyMember);
+						partyMembers.push({ name: partyMember, dps: memberDPS, color: memberColor });
+					}
+				}
+			}
 		}
 	}
-	else
-	{
-		var dps = getDPS(character.name);
-		listString = listString + '<tr align="left"><td align="center">' + character.name + '</td><td>' + dps + '</td></tr>';
+
+	for (const partyMember of partyMembers) {
+		listString += '<tr align="left"><td align="center" style="color:' + partyMember.color + '">' + partyMember.name + '</td><td>' + partyMember.dps + '</td></tr>';
 	}
-	if(parent.party_list != null && character.party != null)
-	{
-		var dps = getDPS();
-		listString = listString + '<tr align="left"><td align="center">' + "Total" + '</td><td>' + dps + '</td></tr>';
+
+	if (parent.party_list != null && character.party != null) {
+		let dps = getDPS();
+		let color = getFontColorByClass("Total");
+		listString += '<tr align="left"><td align="center" style="color:' + color + '">' + 'Total' + '</td><td>' + dps + '</td></tr>';
 	}
-	$('#' + "dpsmetercontent").html(listString);
+
+	$('#' + 'dpsmetercontent').html(listString);
 }
-init_dpsmeter(5)
-function getDPS(partyMember)
-{
+
+// Call the updateMeter function with the classColors object
+updateMeter();
+
+function getFontColorByClass(characterName) {
+	// Handle the current character separately
+	if (characterName === character.name) {
+		return classColors[character.ctype] || '#FFFFFF';
+	}
+
+	let characterClass = null;
+
+	// Search for the character's class in parent.entities
+	if (parent.entities != null) {
+		for (const id in parent.entities) {
+			const entity = parent.entities[id];
+			if (entity.name === characterName) {
+				characterClass = entity.ctype;
+				break;
+			}
+		}
+	}
+
+	// Return the font color based on the character's class
+	return classColors[characterClass] || '#FFFFFF'; // Default to white color (#FFFFFF) if class is not found in the mapping
+}
+
+function getDPS(partyMember) {
 	var sumDamage = 0;
 	var minTime;
 	var maxTime;
 	var entries = 0;
-	for(id in damageLog)
-	{
+	for (id in damageLog) {
 		logEntry = damageLog[id];
-		if(new Date() - logEntry.time < dpsInterval)
-		{
-			if(partyMember == null || logEntry.attacker == partyMember)
-			{
-					if(minTime == null || logEntry.time < minTime)
-					{
-						minTime = logEntry.time;
-					}
-					if(maxTime == null || logEntry.time > maxTime)
-					{
-						maxTime = logEntry.time;
-					}
-					sumDamage += logEntry.damage;
+		if (new Date() - logEntry.time < dpsInterval) {
+			if (partyMember == null || logEntry.attacker == partyMember) {
+				if (minTime == null || logEntry.time < minTime) {
+					minTime = logEntry.time;
+				}
+				if (maxTime == null || logEntry.time > maxTime) {
+					maxTime = logEntry.time;
+				}
+				sumDamage += logEntry.damage;
 				entries++;
 			}
-		}
-		else
-		{
+		} else {
 			damageLog.splice(id, 1);
 		}
 	}
-	if(entries <= 1)
-	{
+	if (entries <= 1) {
 		return 0;
 	}
 	var elapsed = maxTime - minTime;
-	var dps = parseFloat(Math.round((sumDamage/(elapsed/1000)) * 100) / 100).toFixed(2);
+	var dps = parseFloat(Math.round((sumDamage / (elapsed / 1000)) * 100) / 100).toFixed(2);
 	return dps;
 }
-//Clean out an pre-existing listeners
-if (parent.prev_handlersdpsmeter) {
-    for (let [event, handler] of parent.prev_handlersdpsmeter) {
-      parent.socket.removeListener(event, handler);
-    }
+
+function register_dpsmeterhandler(event, handler) {
+	parent.prev_handlersdpsmeter.push([event, handler]);
+	parent.socket.on(event, handler);
 }
-parent.prev_handlersdpsmeter = [];
-//handler pattern shamelessly stolen from JourneyOver
-function register_dpsmeterhandler(event, handler) 
-{
-    parent.prev_handlersdpsmeter.push([event, handler]);
-    parent.socket.on(event, handler);
-};
-function dpsmeterHitHandler(event)
-{
-	if(parent != null)
-	{
+
+function dpsmeterHitHandler(event) {
+	if (parent != null) {
 		var attacker = event.hid;
 		var attacked = event.id;
 		var attackerEntity = parent.entities[attacker];
-		if(attacker == character.name)
-		{
+		if (attacker == character.name) {
 			attackerEntity = character;
 		}
-		if((attackerEntity.party != null || attacker == character.name) || attackerEntity.party == character.party)
-		{
-			if(event.damage != null)
-			{
+		if ((attackerEntity.party != null || attacker == character.name) || attackerEntity.party == character.party) {
+			if (event.damage != null) {
 				var hitEvent = {};
 				hitEvent.damage = event.damage;
 				hitEvent.time = new Date();
@@ -127,4 +158,14 @@ function dpsmeterHitHandler(event)
 		}
 	}
 }
+
+//Clean out any pre-existing listeners
+if (parent.prev_handlersdpsmeter) {
+	for (let [event, handler] of parent.prev_handlersdpsmeter) {
+		parent.socket.removeListener(event, handler);
+	}
+}
+parent.prev_handlersdpsmeter = [];
+
 register_dpsmeterhandler("hit", dpsmeterHitHandler);
+init_dpsmeter(5);

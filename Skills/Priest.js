81db1719d0e9341dfdc0c8d2efafafdd78f8931d
Crowns3bc <44653214+Crowns3bc@users.noreply.github.com>
2023-05-16 09:35:54 -0600

    Zapper v4 & Curse priority

--- Skills/Priest.js
+++ Skills/Priest.js
@@ -77,3 +77,68 @@ async function SkillLoop() {
 	setTimeout(SkillLoop, delay)
 }
 SkillLoop();
+
+
+function calculateCenterPoint(location) {
+  const corners = locations[location];
+  if (!corners || corners.length === 0) {
+    throw new Error(`Invalid or missing corners for location: ${location}`);
+  }
+
+  let sumX = 0;
+  let sumY = 0;
+  const numCorners = corners.length;
+
+  corners.forEach((corner) => {
+    sumX += corner.x;
+    sumY += corner.y;
+  });
+
+  const centerX = sumX / numCorners;
+  const centerY = sumY / numCorners;
+
+  return [centerX, centerY];
+}
+
+
+function get_nearest_monster_v2(args)
+{
+    var min_d=999999,target=null;
+    var min_hp = 999999999;
+    var max_hp = 0;
+
+    if(!args) args={};
+    if(args && args.target && args.target.name) args.target=args.target.name;
+    if(args && args.type=="monster") game_log("get_nearest_monster: you used monster.type, which is always 'monster', use monster.mtype instead");
+    if(args && args.mtype) game_log("get_nearest_monster: you used 'mtype', you should use 'type'");
+
+    for(id in parent.entities)
+    {
+        var current=parent.entities[id];
+        if(current.type!="monster" || !current.visible || current.dead) continue;
+        if(args.type && current.mtype!=args.type) continue;
+        if (args.min_level !== undefined && current.level < args.min_level) continue;
+        if (args.max_level !== undefined && current.level > args.max_level) continue;
+        if(args.target && current.target!=args.target) continue;
+        if(args.no_target && current.target && current.target!=character.name) continue;
+        if (args.cursed && !current.s.cursed) continue;
+        var c_dist;
+        if (args.point_for_distance_check) {
+            c_dist = Math.hypot(args.point_for_distance_check[0] - current.x, args.point_for_distance_check[1] - current.y);
+        } else {
+            c_dist = parent.distance(character,current);
+        }
+        if (args.max_distance !== undefined && c_dist > args.max_distance) continue;
+        if (args.check_min_hp) {
+            var c_hp = current.hp;
+            if (c_hp < min_hp) min_hp = c_hp, target = current;
+            continue;
+        } else if (args.check_max_hp) {
+            var c_hp = current.hp;
+            if (c_hp > max_hp) max_hp = c_hp, target = current;
+            continue;
+        }
+        if(c_dist<min_d) min_d=c_dist,target=current;
+    }
+    return target;
+}

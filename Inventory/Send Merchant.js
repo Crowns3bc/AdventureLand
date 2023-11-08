b505c7ea3e682792079eac04bb6438261aa5ae35
Crowns3bc <44653214+Crowns3bc@users.noreply.github.com>
2023-11-08 00:45:01 -0700

    Update Send Merchant.js

--- Inventory/Send Merchant.js
+++ Inventory/Send Merchant.js
@@ -1,11 +1,11 @@
 function send_merchant() {
 
-	for (var i = 6; i < 20; i++) {
+	for (var i = 0; i < 41; i++) {
         if (character.items[i]) {
             send_item('CrownMerch', i, 9999);
             }
         }
-	if (character.gold > 20000) {
+	if (character.gold > 10000) {
         send_gold("CrownMerch", character.gold - 20000);
     }
 }

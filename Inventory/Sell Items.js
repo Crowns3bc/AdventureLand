69cc662f95e5f8200d508f9c325e0ad923b12cf3
Crowns3bc <44653214+Crowns3bc@users.noreply.github.com>
2024-10-30 11:02:25 -0600

    Update Sell Items.js

--- Inventory/Sell Items.js
+++ Inventory/Sell Items.js
@@ -1,13 +1,20 @@
-sell_whitelist = [ // what you want to sell.
-  'vitearring', 'cclaw', 'hpbelt', 'ringsj', 'hpamulet'
+sell_whitelist = [
+	'vitearring', 'iceskates', 'cclaw', 'hpbelt', 'ringsj', 'hpamulet', 'warmscarf',
+	'quiver', 'snowball', 'vitring', 'wcap', 'wattire', 'wbreeches', 'wshoes',
+	'wgloves', "strring", "dexring", "intring",
 ];
-setInterval(function(){
-  for (let i = 0; i < character.items.length; i++) { 
-      let c = character.items[i];
-      if (c) { 
-          if (c && sell_whitelist.includes(c.name)) {
-              sell(i); 
+function sellItems() {
+	for (let i = 0; i < character.items.length; i++) {
+		let c = character.items[i];
+		if (c) {
+			if (c && sell_whitelist.includes(c.name)) {
+				if (c.p == undefined) {
+					if (c.l != "l") {
+						sell(i);
+					}
+				}
 			}
 		}
 	}
-},1000);
+}
+setInterval(sellItems, 5000);
